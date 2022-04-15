8a71f559d1ef992c661c8ff16ddc53957bdd93b0
e-m-s-y <10279407+e-m-s-y@users.noreply.github.com>
2022-04-15 12:07:00 +0200

    style: prettier

--- plugins/helios-crypto/src/index.ts
+++ plugins/helios-crypto/src/index.ts
@@ -1,10 +1,4 @@
 import * as Builders from "./builders";
 import * as Enums from "./enums";
 import * as Transactions from "./transactions";
-export {
-    Builders,
-    Enums,
-    Transactions
-}
-
-
+export { Builders, Enums, Transactions };
